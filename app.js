710af6ef3bb7035afad0f3ac28a62cf4024c0b25
rodrigofpaixao-arch <rodrigo.f.paixao@gmail.com>
2026-04-23 18:14:35 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const supabase = supabase.createClient(
-  "sb_publishable_MZdbaT0GRL7WqBbC8G0cVQ_fTz9VgPj",
-  "sb_secret_zFn5frdUbjYDFkEWlVtsjQ_Z6qO090b"
-);
-
 async function getRanking() {
   let { data } = await supabase
     .from('pontuacoes')
-    .select('pontos, atiradores(nome, clubes(nome))');
+    .select(`
+      pontos,
+      atiradores(nome, clubes(nome))
+    `);
 
   let ranking = {};
 
